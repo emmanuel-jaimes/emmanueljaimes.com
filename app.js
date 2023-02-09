5db97f0a67f0e64592b1435b6a11e3f290695275
emmanuel jaimes <jaimes.emmanuel@gmail.com>
2023-02-09 02:56:26 -0600

    reworked working structure

---
+++ app.js
@@ -0,0 +1,8 @@
+// window.onscroll = function() {
+//     if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+//         document.querySelector(".menu").style.backgroundColor = "rgba(51, 51, 51, 1)";
+//     } else {
+//         document.querySelector(".menu").style.backgroundColor = "rgba(51, 51, 51, 0)";
+//     }
+// };
+          
